81fd53461862d99558c59a752746faf348ca3f26
Taha-Sid <74993189+Taha-Sid@users.noreply.github.com>
2024-09-26 18:43:57 +0100

    Add files via upload
    Added 3 more functions

--- ToDoAdapter.kt
+++ ToDoAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.todolist
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ToDoAdapter(
     private val todos: MutableList<ToDo>
-) {
+) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() { //ToDoAdapter inheriting from RecyclerView.Adapter
 
-    class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) // class inheriting from rv
+    class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) // ToDoViewHolder inheriting from RecyclerView.ViewHolder
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
